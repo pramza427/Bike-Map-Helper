e8a6772efe9028d8270492fc0056599c79304e0a
pramza427 <pramza427@gmail.com>
2021-04-10 14:43:35 -0500

    Top/Side Bars

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-import {MDCTopAppBar} from '@material/top-app-bar';
-import {MDCTextField} from '@material/textfield';
-import {MDCRipple} from '@material/ripple';
-
-const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+// Instantiation
+const textField = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field'));
 const snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('.mdc-snackbar'));
-MDCRipple.attachTo(document.querySelector('.foo-button'));
+// Set text input bar to ripple
+mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+const list = mdc.list.MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
+const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+// Set topbar and add listener to open side bar
+const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  drawer.open = !drawer.open;
+});
+
+// Left bar list and listener
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const mainContentEl = document.querySelector('.main-content');
+
+document.body.addEventListener('MDCDrawer:closed', () => {
+  mainContentEl.querySelector('input, button').focus();
+});
+
+document.querySelector(".foo-button").addEventListener("click", evt => {
+		document.querySelector(".mdc-snackbar__label").innerHTML = 
+		"You entered: " + document.querySelector("input").value;
+	snackbar.open();
+});
