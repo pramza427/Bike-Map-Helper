cc18b5cebf6bd09fbc73923f50e6063109228714
pramza427 <pramza427@gmail.com>
2021-04-10 13:19:27 -0500

    changed directory

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import {MDCTopAppBar} from './node_modules/@material/top-app-bar';
-import {MDCTextField} from './node_modules/@material/textfield';
-import {MDCRipple} from './node_modules/@material/ripple';
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
 
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 const snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('.mdc-snackbar'));
