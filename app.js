fb45931d2678f55a8e9b54b242b656a452eef848
pramza427 <pramza427@gmail.com>
2021-04-10 13:16:54 -0500

    testing

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ import {MDCTopAppBar} from './node_modules/@material/top-app-bar';
 import {MDCTextField} from './node_modules/@material/textfield';
 import {MDCRipple} from './node_modules/@material/ripple';
 
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
-// Instantiation
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
+const snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('.mdc-snackbar'));
+MDCRipple.attachTo(document.querySelector('.foo-button'));
